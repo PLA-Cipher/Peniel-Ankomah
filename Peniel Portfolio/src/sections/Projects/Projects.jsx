23a99a7cde0952597e9c0acde9d18dd40be92406
PLA-Cipher <169149026+PLA-Cipher@users.noreply.github.com>
2024-12-01 11:31:32 +0000

    This repo contains a portfolio website done using vite and react
    This is a little something about myself

---
+++ Peniel Portfolio/src/sections/Projects/Projects.jsx
@@ -0,0 +1,24 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://www.linkedin.com/posts/era-axis_ecowatch-ghana-awareness-activity-7249679333001236481-1Jio?utm_source=share&utm_medium=member_desktop"
+          h3="Ecowatch"
+          p="Gas emission monitoring device"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
